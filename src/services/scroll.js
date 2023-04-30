764d8ddfe7c7682c0c1ae0927d2de1a9f793f49a
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-04-30 13:33:17 +0200

    +1

--- src/services/scroll.js
+++ src/services/scroll.js
@@ -2,7 +2,7 @@ export function scrollOnLoading() {
   const { height: cardHeight } = document
     .querySelector('.ImageGallery')
     .firstElementChild.getBoundingClientRect();
-  console.log('scroll');
+
   window.scrollBy({
     top: cardHeight * 2.47,
     behavior: 'smooth',
